c7d4ba49bb80978d9b6a3f2607c1db52aabd0056
chiayutsai <37895929+chiayutsai@users.noreply.github.com>
2020-11-17 00:10:14 +0800

    Update 2020-11-16T16:10:05.093Z

---
+++ assets/js/all.js
@@ -0,0 +1,8 @@
+"use strict";
+
+require("../../../node_modules/bootstrap/dist/js/bootstrap");
+
+$(document).ready(function () {
+  console.log('HesSchool Hello!');
+});
+//# sourceMappingURL=all.js.map
