4d8121d373b559124bbde8df7779faecd2e18604
chiayutsai <37895929+chiayutsai@users.noreply.github.com>
2020-11-19 00:27:49 +0800

    Update 2020-11-18T16:27:38.002Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,5 @@
 "use strict";
 
-$('.collapse').collapse();
 var title = $('h2').text();
 console.log(title);
 
@@ -9,4 +8,17 @@ if (title == 'Assignment') {
 } else if (title == 'Admin') {
   $('.sidebar__item:nth-child(5)').addClass('sidebar__item--active');
 }
+
+$('.js-onedit').click(function () {
+  console.log('test');
+  $('.js-editing').css('display', 'block');
+  $('.js-reply').css('display', 'none');
+  $('.js-disaper').css('opacity', '0');
+});
+$('.js-cancle').click(function () {
+  console.log('test');
+  $('.js-editing').css('display', 'none');
+  $('.js-reply').css('display', 'block');
+  $('.js-disaper').css('opacity', '1');
+});
 //# sourceMappingURL=all.js.map
